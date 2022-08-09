cfe028da952231912b9621e9875f796288d7a988
Vladislav <y.surprise@gmail.com>
2022-08-09 23:51:36 +0300

    Homework01

---
+++ Homework01/Homework01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Homework01
+{
+    internal class Program
+    {
+        private static readonly HttpClient client = new();
+
+        public static async Task Main(string[] args)
+        {
+            List<Task<string>> tasks = new();
+            for (int i = 4; i <= 13; i++)
+                tasks.Add(GetPost(i));
+            await Task.WhenAll(tasks);
+            using StreamWriter sw = File.AppendText("file.txt");
+            foreach (Task<string> task in tasks)
+            {
+                if (task.Result != null)
+                {
+                    await sw.WriteLineAsync(task.Result);
+                    await sw.WriteLineAsync();
+                }
+            }
+        }
+
+        private static async Task<string> GetPost(int id)
+        {
+            try
+            {
+                HttpResponseMessage httpResponse = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{id}");
+                httpResponse.EnsureSuccessStatusCode();
+                Response response = await JsonSerializer.DeserializeAsync<Response>(await httpResponse.Content.ReadAsStreamAsync());
+                return response.ToString();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"\nException Caught! id = {id}");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Homework01/Homework01/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework01
+{
+    internal class Response
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        public override string ToString()
+        {
+            string nl = Environment.NewLine;
+            return $"{userId}{nl}{id}{nl}{title}{nl}{body}";
+        }
+    }
+}
