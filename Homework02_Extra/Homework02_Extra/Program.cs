f916136200b33cb4c82d8aa9c8cce99a1ab49d13
Vladislav <y.surprise@gmail.com>
2022-08-11 21:15:11 +0300

    Homework02

---
+++ Homework02_Extra/Homework02_Extra/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Homework02_Extra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = " Предложение один Теперь предложение два Предложение три ";
+            Console.WriteLine(str);
+            Regex regex = new(@" (\p{Lu})");
+            string res = regex.Replace(str.Trim(), ". $1") + '.';
+            Console.WriteLine(res);
+        }
+    }
+}
