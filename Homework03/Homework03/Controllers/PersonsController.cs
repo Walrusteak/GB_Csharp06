6d05399671767cfcecdff6fa26b8bf6114ded3aa
Vladislav <y.surprise@gmail.com>
2022-08-21 01:22:47 +0300

    Homework03

---
+++ Homework03/Homework03/Controllers/PersonsController.cs
@@ -0,0 +1,56 @@
+﻿using Homework03.Models.Controller;
+using Homework03.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework03.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IService<Person> _service;
+
+        public PersonsController(IService<Person> service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            return Ok(_service.Get(id));
+        }
+
+        [HttpGet("search")]
+        public IActionResult Search([FromQuery] string searchTerm)
+        {
+            return Ok(_service.Search(searchTerm));
+        }
+
+        [HttpGet()]
+        public IActionResult GetSkip([FromQuery] int skip, int take)
+        {
+            return Ok(_service.Get(skip, take));
+        }
+
+        [HttpPost()]
+        public IActionResult Add([FromBody] Person person)
+        {
+            _service.Add(person);
+            return Ok();
+        }
+
+        [HttpPut()]
+        public IActionResult Update([FromBody] Person person)
+        {
+            _service.Delete(person);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            return Ok(_service.Get(id));
+        }
+    }
+}
---
+++ Homework03/Homework03/Services/IService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Homework03.Services
+{
+    public interface IService<T> where T : class
+    {
+        T Get(int id);
+        IEnumerable<T> Search(string term);
+        IEnumerable<T> Get(int skip, int take);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        void Delete(T entity);
+    }
+}
